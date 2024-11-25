b183e4794b2d8f901364dfc6174fecf041e91cac
cieloflorecab <florezcielo87@gmail.com>
2024-11-25 09:55:41 -0500

    APIs V2 (Integracion SELECT, insert, delete, update Department)

--- Servicios/APIs.py
+++ Servicios/APIs.py
@@ -1,6 +1,7 @@
 from Repositorio.Conexion_BD import Conexion
 import flask;
 import sys;
+import datetime;
 
 app = flask.Flask(__name__)
                
@@ -24,3 +25,77 @@ def SELECT(entrada: str) -> str :
     except:
         Datos ["Send"] = sys.exc_info();
         return flask.jsonify(Datos );    
+
+
+#Table Departamento
+# http://localhost:4040/departamento/InsertarDepartamento   
+@app.route('/departamento/InsertarDepartamento', methods=["POST"])
+def InsertarDepartamentoAPI():
+    respuesta = {}
+    try:
+        # Parsear datos de la solicitud
+        datos = flask.request.get_json()
+
+        # Validar datos de entrada
+        if not all(k in datos for k in ("id_departamento", "nombre")):
+            respuesta["Response"] = "Faltan parámetros en la solicitud."
+            return flask.jsonify(respuesta), 400
+
+        # Obtener parámetros
+        id_departamento = datos["id_departamento"]
+        nombre = datos["nombre"]
+
+        # Llamar a la función InsertarDepartamento
+        conexion = Conexion()
+        conexion.Insertar_Departamento(id_departamento=id_departamento, nombre=nombre)
+
+        respuesta["Response"] = "Ok"
+        respuesta["ID_Departamento"] = id_departamento
+        return flask.jsonify(respuesta), 201
+    except Exception as e:
+        respuesta["Error"] = str(e)
+        return flask.jsonify(respuesta), 500
+    
+   
+# http://localhost:4040/departamento/EliminarDepartamento/
+@app.route('/departamento/EliminarDepartamento/<int:ID_Departamento>', methods=["DELETE"])
+def EliminarEspecieAPI(ID_Departamento):
+    respuesta = {}
+    try:
+        # Llamar a la función Eliminar_Departamento
+        conexion = Conexion()
+        respuesta = conexion.Eliminar_Departamento(ID_Departamento)
+
+        status_code = 200 if respuesta["Response"] == "Ok" else 404
+        return flask.jsonify(respuesta), status_code
+    except Exception as e:
+        respuesta["Response"] = "Error"
+        respuesta["Mensaje"] = str(e)
+        return flask.jsonify(respuesta), 500
+
+# http://localhost:4040/departamento/ActualizarDepartamento
+@app.route('/departamento/ActualizarDepartamento', methods=["PUT"])
+def ActualizarDepartamentoAPI():
+    respuesta = {}
+    try:
+        # Parsear datos de la solicitud
+        datos = flask.request.get_json()
+
+        # Validar datos de entrada
+        if not all(k in datos for k in ("id", "nombre")):
+            respuesta["Response"] = "Faltan parámetros en la solicitud."
+            return flask.jsonify(respuesta), 400
+
+        # Convertir datos
+        ID_Departamento = int(datos["id"])
+      
+        # Llamar a la función ActualizarEspecie
+        conexion = Conexion()
+        respuesta = conexion.Actualizar_Departamento(ID_Departamento, datos["nombre"])
+
+        status_code = 200 if respuesta["Response"] == "Ok" else 404
+        return flask.jsonify(respuesta), status_code
+    except Exception as e:
+        respuesta["Response"] = "Error"
+        respuesta["Mensaje"] = str(e)
+        return flask.jsonify(respuesta), 500
